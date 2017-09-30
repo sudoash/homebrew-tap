9c45e6b4e39f409364f0019016007b0412373356
Ashley Snowdon <ashley.snowdon@gmail.com>
2017-09-30 16:24:46 +0100

    Updated sha256 for docker-machine-ipconfig

--- Formula/docker-machine-ipconfig.rb
+++ Formula/docker-machine-ipconfig.rb
@@ -2,7 +2,7 @@ class DockerMachineIpconfig < Formula
   desc "Allows one to toggle between static and DHCP docker-machine network interface settings"
   homepage "https://github.com/fivestars/docker-machine-ipconfig"
   url "https://raw.githubusercontent.com/fivestars/docker-machine-ipconfig/master/docker-machine-ipconfig"
-  sha256 "e7bf641c7be869a0ccddaac8093453e78986cac408f311b4a049533dd567ea7f"
+  sha256 "283462823efcc91e8db29fd7d6de7b76a6432fd060e6c35c007f3ee40bfeaf89"
 
   def install
     bin.install "docker-machine-ipconfig" => "docker-machine-ipconfig"
