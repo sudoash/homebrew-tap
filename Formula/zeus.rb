b415101fdfb0f8e96e4b8317d43a59f2f4c5e5d6
Ashley Snowdon <ashley.snowdon@gmail.com>
2020-05-08 15:45:43 +0100

    Bumped to version 1.2.0

--- Formula/zeus.rb
+++ Formula/zeus.rb
@@ -1,9 +1,9 @@
 class Zeus < Formula
   desc "Facilitates interaction between docker, docker-machine and docker-compose"
   homepage "https://github.com/sudoash/zeus"
-  url "https://github.com/sudoash/zeus/archive/1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "1b8df7cf432cf37dcec178d19ccbee289a683fecdde2e9f2914f195cf1f9b583"
+  url "https://github.com/sudoash/zeus/archive/1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "50dd7567edb2ec458d45b73eb79f803d1eaa6c349c6385018aad697e808fd295"
 
   def install
     bin.install "zeus" => "zeus"
